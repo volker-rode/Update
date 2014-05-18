0c3208472262b46d2b941dad24781d63d8ce3cd6
Tim Moxter <tim.moxter@innobix.com>
2014-05-19 01:46:46 +0200

    - related to issue #191   * multi value flag is supported from attribute CI

--- src/main/java/org/mxupdate/update/datamodel/AbstractAttribute_mxJPO.java
+++ src/main/java/org/mxupdate/update/datamodel/AbstractAttribute_mxJPO.java
@@ -146,6 +146,8 @@ public abstract class AbstractAttribute_mxJPO<CLASS extends AbstractAttribute_mx
      */
     private final String attrTypeList;
 
+    /** Flag that the attribute has multiple values. */
+    private boolean multiValue = false;
     /** Flag that the attribute value is reset on clone. */
     private boolean resetOnClone = false;
     /** Flag that the attribute value is reset on revision. */
@@ -200,9 +202,11 @@ public abstract class AbstractAttribute_mxJPO<CLASS extends AbstractAttribute_mx
      * <ul>
      * <li>assigned {@link #rules}</li>
      * <li>{@link #defaultValue default value}</li>
+     * <li>{@link #multiValue multiple value} flag</li>
      * <li>{@link #resetOnClone reset on clone} flag</li>
      * <li>{@link #resetOnRevision reset on revision} flag</li>
-     * <li>defined {@link #ranges}, {@link #rangeProgramRef program ranges} and
+     * <li>defined {@link #rangesStack ranges},
+     *     {@link #rangeProgramRef program ranges} and
      *     their {@link #rangeProgramInputArguments input arguments}</li>
      * </ul>
      *
@@ -224,6 +228,15 @@ public abstract class AbstractAttribute_mxJPO<CLASS extends AbstractAttribute_mx
         } else if ("/accessRuleRef".equals(_url))  {
             this.rules.add(_content);
             parsed = true;
+        } else if ("/attrValueType".equals(_url))  {
+            if ("1".equals(_content))  {
+                this.multiValue = true;
+                parsed = true;
+            } else if ("0".equals(_content))  {
+                parsed = true;
+            } else  {
+                parsed = false;
+            }
         } else if ("/defaultValue".equals(_url))  {
             this.defaultValue = _content;
             parsed = true;
@@ -317,10 +330,12 @@ public abstract class AbstractAttribute_mxJPO<CLASS extends AbstractAttribute_mx
      * must be done in front or otherwise some data is lost. Following
      * information is written
      * <ul>
+     * <li>flag &quot;{@link #multiValue multiple value}&quot; (if parameter
+     *     {@link ValueKeys#DMAttrSupportsFlagMultiValue} is defined)</li>
      * <li>flag &quot;{@link #resetonclone}&quot; (if parameter
-     *     {@link #PARAM_SUPPORT_FLAG_RESET_ON_CLONE} is defined)</li>
+     *     {@link ValueKeys#DMAttrSupportsFlagResetOnClone} is defined)</li>
      * <li>flag &quot;{@link #resetOnRevision}&quot; (if parameter
-     *     {@link #PARAM_SUPPORT_FLAG_RESET_ON_REVISION} is defined)</li>
+     *     {@link ValueKeys#DMAttrSupportsFlagResetOnRevision} is defined)</li>
      * <li>{@link #writeAttributeSpecificValues(ParameterCache_mxJPO, Appendable)
      *     attribute specific properties and flags}</li>
      * <li>all assigned {@link #rules}</li>
@@ -349,6 +364,9 @@ public abstract class AbstractAttribute_mxJPO<CLASS extends AbstractAttribute_mx
         _out.append("updateAttribute \"${NAME}\"  {\n")
             .append("  description \"").append(StringUtil_mxJPO.convertTcl(this.getDescription())).append("\"\n")
             .append("  ").append(this.isHidden() ? "" : "!").append("hidden\n");
+        if (_paramCache.getValueBoolean(ValueKeys.DMAttrSupportsFlagMultiValue))  {
+            _out.append("  ").append(this.multiValue ? "" : "!").append("multivalue\n");
+        }
         if (_paramCache.getValueBoolean(ValueKeys.DMAttrSupportsFlagResetOnClone))  {
             _out.append("  ").append(this.resetOnClone ? "" : "!").append("resetonclone\n");
         }
@@ -426,24 +444,9 @@ public abstract class AbstractAttribute_mxJPO<CLASS extends AbstractAttribute_mx
     }
 
     /**
-     * The method overwrites the original method to append the MQL statements
-     * in the <code>_preMQLCode</code> to reset this attribute. This includes:
-     * <ul>
-     * <li>set to not hidden</li>
-     * <li>flag &quot;resetonclone&quot; is disabled (if parameter
-     *     {@link #PARAM_SUPPORT_FLAG_RESET_ON_CLONE} is defined)</li>
-     * <li>flag &quot;resetonrevision&quot; is disabled (if parameter
-     *     {@link #PARAM_SUPPORT_FLAG_RESET_ON_REVISION} is defined)</li>
-     * <li>flag &quot;multiline&quot; is disabled for string attributes</li>
-     * <li>reset description and default value</li>
-     * <li>remove all ranges</li>
-     * <li>define the TCL procedure {@link #TCL_PROCEDURE} to set the
-     *     dimension</li>
-     * <li>if the dimension is not updated within the TCL update file (tested
-     *     with {@link #dimensionUpdated}) method
-     *     {@link #updateDimension(ParameterCache_mxJPO, String)} is called
-     *     </li>
-     * </ul>
+     * The method overwrites the original method to add the TCL procedure
+     * {@link #TCL_PROCEDURE} so that the attribute could be updated with
+     * {@link #jpoCallExecute(ParameterCache_mxJPO, String...)}.
      *
      * @param _paramCache       parameter cache
      * @param _preMQLCode       MQL statements which must be called before the
@@ -483,20 +486,17 @@ public abstract class AbstractAttribute_mxJPO<CLASS extends AbstractAttribute_mx
     }
 
     /**
-     * The method is called from the TCL update code to define the dimension
-     * for this attribute. If the correct use case is defined method
+     * The method is called from the TCL update code to define the this
+     * attribute. If the correct use case is defined method
      * {@link #updateDimension(ParameterCache_mxJPO, String)} is called.
      *
      * @param _paramCache   parameter cache
      * @param _args         first index defines the use case (must be
-     *                      &quot;defineAttrDimension&quot; that the dimension
-     *                      of the attribute is updated); second index the name
-     *                      of the attribute to update; third index the new
-     *                      dimension to set
+     *                      &quot;updateAttribute&quot; that the attribute
+     *                      is updated); second index the name of the attribute
+     *                      to update
      * @throws Exception if the update of the dimension failed or for all other
      *                   use cases from super JPO call
-     * @see #updateDimension(ParameterCache_mxJPO, String)
-     * @see #dimensionUpdated
      */
     @Override()
     public void jpoCallExecute(final ParameterCache_mxJPO _paramCache,
@@ -552,6 +552,15 @@ public abstract class AbstractAttribute_mxJPO<CLASS extends AbstractAttribute_mx
         DeltaUtil_mxJPO.calcValueDelta(_mql, "default",         target.defaultValue,     this.defaultValue);
         DeltaUtil_mxJPO.calcFlagDelta(_mql,  "hidden",          target.isHidden(),       this.isHidden());
 
+        if (_paramCache.getValueBoolean(ValueKeys.DMAttrSupportsFlagMultiValue))  {
+            if (!this.multiValue)  {
+                DeltaUtil_mxJPO.calcFlagDelta(_mql, "multivalue", target.multiValue, this.multiValue);
+            } else if (!target.multiValue)  {
+                throw new UpdateException_mxJPO(
+                        UpdateException_mxJPO.Error.ABSTRACTATTRIBUTE_UPDATE_MULTIVALUEFLAG_UPDATED,
+                        this.getName());
+            }
+        }
         if (_paramCache.getValueBoolean(ValueKeys.DMAttrSupportsFlagResetOnClone))  {
             DeltaUtil_mxJPO.calcFlagDelta(_mql,  "resetonclone",    target.resetOnClone,     this.resetOnClone);
         }
